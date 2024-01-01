be3eb0a8e4ff20f4e6f06f61a0ae236e8d5855c6
Firdous <firdousrahaman42@gmail.com>
2024-01-01 22:26:22 +0530

    Added Form validation

--- contact-us/script.js
+++ contact-us/script.js
@@ -1,13 +1,44 @@
-const inputs = document.getElementsByTagName('input');
-const textarea = document.getElementsByTagName('textarea');
+const submitButton = document.querySelector('#btn');
 
-console.log(input);
-inputs.forEach((input) => {
-    input.addEventListener('click', () => {
-        input.classList('.active');
-    });
+const forminputs = [];
+const empty = [];
+
+let obj = {
+    name: '',
+    email: '',
+    phone: '',
+    message: ''
+};
+
+submitButton.addEventListener('click', () => {
+    const inputs = document.querySelectorAll('.form_input');
+    const input_paras = document.querySelectorAll('.input_para');
+    let i = 0;
+
+    const arrinput = Array.from(inputs)
+    const arrInputPara = Array.from(input_paras);
+
+    for (const input of arrinput) {
+        if (input.value === '') {
+            empty.push(i);
+        }
+        i++;
+    }
+
+    if (empty.length === 0) {
+        alert("Form submitted Succesfully");
+        arrinput.forEach((input) => {
+            input.value = '';
+        });
+    }
+    else {
+        for (const index of empty) {
+            arrInputPara[index].classList = 'active';
+        }
+    }
 });
 
-textarea.addEventListener('click', () => {
-    textarea.classList('.active')
-});
+
+// console.log(inputs)
+
+
